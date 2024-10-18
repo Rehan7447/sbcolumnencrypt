17259020e2435fce039ab066daf27e744748a58d
Rehan <m.rd7447@gmail.com>
2024-10-18 15:08:39 +0500

    initialize project

---
+++ src/main/java/com/testEncryption/testEncrypt/TestEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.testEncryption.testEncrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEncryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testEncryption/testEncrypt/TestEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testEncryption.testEncrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
