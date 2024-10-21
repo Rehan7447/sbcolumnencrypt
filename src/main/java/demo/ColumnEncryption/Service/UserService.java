a944d8eb4348cf173574addae76844a952162902
Rehan <m.rd7447@gmail.com>
2024-10-21 19:19:35 +0500

    add controller and service for basic user

--- src/main/java/demo/ColumnEncryption/Service/UserService.java
+++ src/main/java/demo/ColumnEncryption/Service/UserService.java
@@ -9,4 +9,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User createUser(UserCreateRequest request) {
+        // create user
+        User newUser = new User();
+        newUser.setUsername(request.username);
+        newUser.setPassword(request.password);
+        newUser.setEmail(request.email);
+        newUser.setGender(request.gender);
+        return userRepository.save(newUser);
+    }
+
+    public User getUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUserById(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUserById(Integer id, UserCreateRequest request) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return null;
+        }
+        user.setUsername(request.username);
+        user.setPassword(request.password);
+        user.setEmail(request.email);
+        user.setGender(request.gender);
+
+        return userRepository.save(user);
+    }
 }
